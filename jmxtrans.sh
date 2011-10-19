2f01bce8453fc139b90f850ad10f5f056210e924
hgomez <henri.gomez@gmail.com>
2011-10-19 06:46:18 +0000

    Make default log level configurable from the outside. By default still in DEBUG

--- jmxtrans.sh
+++ jmxtrans.sh
@@ -25,6 +25,9 @@ MAX_PERM_SIZE=${MAX_PERM_SIZE:-"384"}
 CPU_CORES=${CPU_CORES:-"1"}
 IO_FRACTION=${IO_FRACTION:-"85"}
 JMX_PORT=${JMX_PORT:-"2101"}
+JMXTRANS_LOG_LEVEL=${JMXTRANS_LOG_LEVEL:-"debug"}
+JMXTRANS_OPTS="$JMXTRANS_OPTS -Djmxtrans.log.level=${JMXTRANS_LOG_LEVEL}"
+
 
 MONITOR_OPTS=${MONITOR_OPTS:-"-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=${JMX_PORT}"}
 GC_OPTS=${GC_OPTS:-"-Xms${HEAP_SIZE}M -Xmx${HEAP_SIZE}M -XX:+UseConcMarkSweepGC -XX:NewRatio=${NEW_RATIO} -XX:NewSize=${NEW_SIZE}m -XX:MaxNewSize=${NEW_SIZE}m -XX:MaxTenuringThreshold=16 -XX:GCTimeRatio=9 -XX:PermSize=${PERM_SIZE}m -XX:MaxPermSize=${MAX_PERM_SIZE}m -XX:+UseTLAB -XX:CMSInitiatingOccupancyFraction=${IO_FRACTION} -XX:+CMSIncrementalMode -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=${CPU_CORES} -Dsun.rmi.dgc.server.gcInterval=28800000 -Dsun.rmi.dgc.client.gcInterval=28800000"}
