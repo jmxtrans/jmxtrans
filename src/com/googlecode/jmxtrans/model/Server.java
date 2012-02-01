46cd063f98784d7f5362bce3e7032ec50e81bd67
Marcos Lois <marcos.lois@gmail.com>
2012-02-01 02:51:38 +0100

    Removed comment.

--- src/com/googlecode/jmxtrans/model/Server.java
+++ src/com/googlecode/jmxtrans/model/Server.java
@@ -199,7 +199,6 @@ public class Server {
 	 * The jmx url to connect to. If null, it builds this from host/port with a
 	 * standard configuration. Other JVM's may want to set this value.
 	 */
-//	@JsonIgnore
 	public String getUrl() {
 		if (this.url == null) {
 			if ((this.host == null) || (this.port == null)) {
