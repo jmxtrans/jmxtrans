2ca0a650513e3f25ed9adc990142fe759af76d29
Balazs Kossovics <bko@witbe.net>
2013-04-11 11:27:45 +0200

    adding limited support for the typeNames variable

--- src/com/googlecode/jmxtrans/model/output/OpenTSDBWriter.java
+++ src/com/googlecode/jmxtrans/model/output/OpenTSDBWriter.java
@@ -4,6 +4,7 @@ import com.googlecode.jmxtrans.model.Query;
 import com.googlecode.jmxtrans.model.Result;
 import com.googlecode.jmxtrans.util.BaseOutputWriter;
 import com.googlecode.jmxtrans.util.ValidationException;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,7 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -28,32 +30,53 @@ public class OpenTSDBWriter extends BaseOutputWriter {
     private Integer port;
     private Map<String, String> tags;
 
-    String addTags(String resultString) {
-        String tagFormat = " %s=%s";
-        for (Map.Entry<String, String> tagEntry : tags.entrySet()) {
-            resultString += String.format(tagFormat, tagEntry.getKey(), tagEntry.getValue());
-        }
+    String addTags(String resultString) throws UnknownHostException {
+        resultString = addTag(resultString, "host", java.net.InetAddress.getLocalHost().getHostName());
+        if (tags != null)
+            for (Map.Entry<String, String> tagEntry : tags.entrySet()) {
+                resultString = addTag(resultString, tagEntry.getKey(), tagEntry.getValue());
+            }
         return resultString;
     }
 
-    List<String> resultParser(Result result) {
+    String addTag(String resultString, String tagName, String tagValue) {
+        String tagFormat = " %s=%s";
+        resultString += String.format(tagFormat, tagName, tagValue);
+        return resultString;
+    }
+
+    String getResultString(String className, String attributeName, long epoch, Object value) {
+        String resultStringFormat = "put %s.%s %d %s";
+        return String.format(resultStringFormat, className, attributeName, epoch, value);
+    }
+
+    String getResultString(String className, String attributeName, long epoch, Object value, String tagName, String tagValue) {
+        String taggedResultStringFormat = "put %s.%s %d %s %s=%s";
+        return String.format(taggedResultStringFormat, className, attributeName, epoch, value, tagName, tagValue);
+    }
+
+    List<String> resultParser(Result result) throws UnknownHostException {
         List<String> resultStrings = new LinkedList<String>();
         Map<String, Object> values = result.getValues();
         if (values == null)
             return resultStrings;
-        String resultStringFormat = "put %s.%s %d %s";
-        String taggedResultStringFormat = "put %s.%s %d %s type=%s";
 
         String attributeName = result.getAttributeName();
         String className = result.getClassNameAlias() == null ? result.getClassName() : result.getClassNameAlias();
         if (values.containsKey(attributeName) && values.size() == 1) {
-            String resultString = String.format(resultStringFormat, className, attributeName, (long)(result.getEpoch()/1000L), values.get(attributeName));
+            String resultString = getResultString(className, attributeName, (long)(result.getEpoch()/1000L), values.get(attributeName));
             resultString = addTags(resultString);
+            if (getTypeNames().size() > 0) {
+                resultString = addTag(resultString, StringUtils.join(getTypeNames(), ""), getConcatedTypeNameValues(result.getTypeName()));
+            }
             resultStrings.add(resultString);
         } else {
             for (Map.Entry<String, Object> valueEntry: values.entrySet() ) {
-                String resultString = String.format(taggedResultStringFormat, className, attributeName, (long)(result.getEpoch()/1000L), valueEntry.getValue(), valueEntry.getKey());
+                String resultString = getResultString(className, attributeName, (long)(result.getEpoch()/1000L), valueEntry.getValue(), "subType", valueEntry.getKey());
                 resultString = addTags(resultString);
+                if (getTypeNames().size() > 0) {
+                    resultString = addTag(resultString, StringUtils.join(getTypeNames(), ""), getConcatedTypeNameValues(result.getTypeName()));
+                }
                 resultStrings.add(resultString);
             }
         }
@@ -69,7 +92,7 @@ public class OpenTSDBWriter extends BaseOutputWriter {
         for (Result result : query.getResults()) {
             for(String resultString: resultParser(result)) {
                 if (isDebugEnabled())
-                    log.debug(resultString);
+                    System.out.println(resultString);
                 out.writeBytes(resultString + "\n");
             }
         }
