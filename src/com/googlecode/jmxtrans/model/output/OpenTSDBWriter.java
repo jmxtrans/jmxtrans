436b97eb066125d12b271f534f2c57d9f8c888b4
Balázs Kossovics <kossovics@gmail.com>
2013-07-30 22:58:36 +0200

    using long-lived connections to OpenTSDB, wrapping IO operations with try/catch blocks and reading and logging server errors sent back by OpenTSDB

--- src/com/googlecode/jmxtrans/model/output/OpenTSDBWriter.java
+++ src/com/googlecode/jmxtrans/model/output/OpenTSDBWriter.java
@@ -4,6 +4,7 @@ import com.googlecode.jmxtrans.model.Query;
 import com.googlecode.jmxtrans.model.Result;
 import com.googlecode.jmxtrans.util.BaseOutputWriter;
 import com.googlecode.jmxtrans.util.ValidationException;
+import com.googlecode.jmxtrans.util.LifecycleException;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +17,7 @@ import java.net.UnknownHostException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.io.IOException;
 
 /**
  * Created by Balazs Kossovics <bko@witbe.net>
@@ -23,13 +25,12 @@ import java.util.Map;
  * Time: 6:00 PM
  */
 public class OpenTSDBWriter extends BaseOutputWriter {
-
     private static final Logger log = LoggerFactory.getLogger(OpenTSDBWriter.class);
-
     private String host;
     private Integer port;
     private Map<String, String> tags;
     private String tagName;
+    private Socket socket;
 
     String addTags(String resultString) throws UnknownHostException {
         resultString = addTag(resultString, "host", java.net.InetAddress.getLocalHost().getHostName());
@@ -86,26 +87,68 @@ public class OpenTSDBWriter extends BaseOutputWriter {
 
     @Override
     public void doWrite(Query query) throws Exception {
-        Socket socket = new Socket(host, port);
-        DataOutputStream out = new DataOutputStream(
-                socket.getOutputStream());
+        DataOutputStream out;
+        try {
+            out = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            log.error("error getting the output stream", e);
+            throw e;
+        }
 
         for (Result result : query.getResults()) {
             for(String resultString: resultParser(result)) {
                 if (isDebugEnabled())
                     System.out.println(resultString);
-                out.writeBytes(resultString + "\n");
+                try {
+                    out.writeBytes(resultString + "\n"); 
+                } catch (IOException e) {
+                    log.error("error writing result to the output stream", e);
+                    throw e;
+                }
             }
         }
-
-        socket.close();
+        try {
+            out.flush();
+        } catch (IOException e) {
+            log.error("flush failed");
+            throw e;
+        }
+        InputStreamReader socketInputStream = new InputStreamReader(socket.getInputStream());
+        BufferedReader bufferedSocketInputStream = new BufferedReader(socketInputStream);
+        String line;
+        while (socketInputStream.ready() && (line = bufferedSocketInputStream.readLine()) != null) {
+            log.warn("OpenTSDB says: " + line); 
+        }
     }
 
     @Override
     public void validateSetup(Query query) throws ValidationException {
+    }
+
+    @Override
+    public void start() throws LifecycleException {
         host = (String) this.getSettings().get(HOST);
         port = (Integer) this.getSettings().get(PORT);
         tags = (Map<String, String>) this.getSettings().get("tags");
         tagName = this.getStringSetting("tagName", "type");
+        try {
+            socket = new Socket(host, port);
+        } catch(UnknownHostException e) {
+            log.error("error opening socket to OpenTSDB", e);
+            throw new LifecycleException(e);
+        } catch(IOException e) {
+            log.error("error opening socket to OpenTSDB", e);
+            throw new LifecycleException(e);
+        }
+    }
+
+    @Override
+    public void stop() throws LifecycleException {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            log.error("error closing socket to OpenTSDB");
+            throw new LifecycleException(e);
+        }
     }
 }
