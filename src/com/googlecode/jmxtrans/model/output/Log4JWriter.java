b762635123966295f91e56f036b8fd2a1faa8138
Jon Stevens <latchkey@gmail.com>
2014-07-08 22:41:14 -0700

    fix build for log4jwriter

--- src/com/googlecode/jmxtrans/model/output/Log4JWriter.java
+++ src/com/googlecode/jmxtrans/model/output/Log4JWriter.java
@@ -1,26 +1,25 @@
 package com.googlecode.jmxtrans.model.output;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import org.apache.log4j.Logger;
-import org.apache.log4j.MDC;
-
 import com.googlecode.jmxtrans.model.Query;
 import com.googlecode.jmxtrans.model.Result;
 import com.googlecode.jmxtrans.util.BaseOutputWriter;
 import com.googlecode.jmxtrans.util.JmxUtils;
 import com.googlecode.jmxtrans.util.ValidationException;
+import org.apache.log4j.Logger;
+import org.apache.log4j.MDC;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 
 /**
  * 
- * A writer for Log4J. It may be a nice way to send JMX metrics to Logstash for example. </br> </br>
+ * A writer for Log4J. It may be a nice way to send JMX metrics to Logstash for example. <br /> <br />
  * 
- * Here is an example of MDC variables that are set by this class. </br> </br> server: localhost_9003 </br> metric:
- * sun_management_MemoryImpl.HeapMemoryUsage_committed </br> value: 1251999744 </br> resultAlias: myHeapMemoryUsage
- * </br> attributeName: HeapMemoryUsage </br> key: committed </br> epoch: 1388343325728 </br>
+ * Here is an example of MDC variables that are set by this class. <br> <br> server: localhost_9003 <br /> metric:
+ * sun_management_MemoryImpl.HeapMemoryUsage_committed <br /> value: 1251999744 <br /> resultAlias: myHeapMemoryUsage
+ * <br /> attributeName: HeapMemoryUsage <br /> key: committed <br /> epoch: 1388343325728 <br />
  * 
  * @author Yannick Robin
  */
@@ -28,14 +27,12 @@ public class Log4JWriter extends BaseOutputWriter
 {
 	private static Logger LOG;
 
-	public Log4JWriter()
-	{
-	}
+	/** */
+	public Log4JWriter() { }
 
 	/**
 	 * Get the logger
 	 */
-	@Override
 	public void validateSetup(final Query query) throws ValidationException
 	{
 		String loggerName = (String) this.getSettings().get("logger");
@@ -51,7 +48,6 @@ public class Log4JWriter extends BaseOutputWriter
 	/**
 	 * Set the log context and log
 	 */
-	@Override
 	public void doWrite(final Query query) throws Exception
 	{
 		final List<String> typeNames = getTypeNames();
@@ -77,7 +73,7 @@ public class Log4JWriter extends BaseOutputWriter
 						}
 
 						MDC.put("server", alias);
-						MDC.put("metric", JmxUtils.getKeyString2(query, result, values, typeNames, null));
+						MDC.put("metric", JmxUtils.getKeyString(query, result, values, typeNames, null));
 						MDC.put("value", values.getValue());
 						if (result.getClassNameAlias() != null)
 						{
