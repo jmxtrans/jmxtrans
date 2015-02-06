61b0ff49d6ab4525b625133a0a39e6a51ca785dd
gehel <guillaume.lederrey@gmail.com>
2015-02-06 22:22:21 +0100

    Configuring publication of artifacts to Sonatype

---
+++ travis-script.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -ev
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  if [ "$TRAVIS_BRANCH" == "master" ]; then
+    echo "Building master"
+    mvn deploy --settings target/travis/settings.xml -B -V -PwithMutationTests
+  else
+    echo "Building feature branch"
+    mvn verify --settings target/travis/settings.xml -B -V -PwithMutationTests
+  fi
+else
+  echo "Building pull request"
+  mvn verify --settings target/travis/settings.xml -B -V -PwithMutationTests
+fi
