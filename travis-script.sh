187cd930c051f0d1d5fcc08859fcf8cbd9a25d67
gehel <guillaume.lederrey@gmail.com>
2015-02-07 00:14:01 +0100

    Let's build RPM on travis ...

--- travis-script.sh
+++ travis-script.sh
@@ -4,7 +4,7 @@ set -ev
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   if [ "$TRAVIS_BRANCH" == "master" ]; then
     echo "Building master"
-    mvn deploy --settings target/travis/settings.xml -B -V -PwithMutationTests
+    mvn deploy --settings target/travis/settings.xml -B -V -PwithMutationTests,rpm
   else
     echo "Building feature branch"
     mvn verify --settings target/travis/settings.xml -B -V -PwithMutationTests
