598e73082b123a162d3318d50b748864ca187d34
gehel <guillaume.lederrey@gmail.com>
2015-03-29 13:13:25 +0200

    Settings need to be accessed by absolute path
    release plugin executes the release from a fresh checkout, so not from the same directory as standard build. Relative path to settings will not work.

--- travis-script.sh
+++ travis-script.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 set -ev
 
+MVN_SETTINGS=`pwd`/target/travis/settings.xml
+
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   if [ "$TRAVIS_BRANCH" == "master" ]; then
     echo "Building master"
-    mvn deploy --settings target/travis/settings.xml -B -V -PwithMutationTests,gpg,rpm,deb
+    mvn deploy --settings ${MVN_SETTINGS} -B -V -PwithMutationTests,gpg,rpm,deb
   elif [ "$TRAVIS_BRANCH" == "release" ]; then
     if [[ `git log --format=%B -n 1` == *"[maven-release-plugin]"* ]]; then
       echo "Do not release commits created by maven release plugin"
     else
       echo "Building release"
-      mvn release:prepare --settings target/travis/settings.xml -B -V -PwithMutationTests,gpg,rpm,deb -Darguments="--settings target/travis/settings.xml"
-      mvn release:perform --settings target/travis/settings.xml -B -V -PwithMutationTests,gpg,rpm,deb -Darguments="--settings target/travis/settings.xml"
+      mvn release:prepare --settings ${MVN_SETTINGS} -B -V -PwithMutationTests,gpg,rpm,deb -Darguments="--settings ${MVN_SETTINGS}"
+      mvn release:perform --settings ${MVN_SETTINGS} -B -V -PwithMutationTests,gpg,rpm,deb -Darguments="--settings ${MVN_SETTINGS}"
       git checkout master
       git merge release
       git push origin HEAD
     fi
   else
     echo "Building feature branch"
-    mvn verify --settings target/travis/settings.xml -B -V -PwithMutationTests,rpm,deb
+    mvn verify --settings ${MVN_SETTINGS} -B -V -PwithMutationTests,rpm,deb
   fi
 else
   echo "Building pull request"
-  mvn verify --settings target/travis/settings.xml -B -V -PwithMutationTests,rpm,deb
+  mvn verify --settings ${MVN_SETTINGS} -B -V -PwithMutationTests,rpm,deb
 fi
