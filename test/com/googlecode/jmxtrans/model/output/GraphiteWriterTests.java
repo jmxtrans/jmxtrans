90ac10e62519202687b26e0220ad3b05237e7bf2
Conor Beverland <conorbev@users.noreply.github.com>
2015-04-09 14:43:01 -0700

    Added a test for the new useObjDomain option.

--- test/com/googlecode/jmxtrans/model/output/GraphiteWriterTests.java
+++ test/com/googlecode/jmxtrans/model/output/GraphiteWriterTests.java
@@ -5,11 +5,13 @@ import com.googlecode.jmxtrans.model.Query;
 import com.googlecode.jmxtrans.model.Result;
 import com.googlecode.jmxtrans.model.Server;
 import com.googlecode.jmxtrans.model.ValidationException;
+
 import org.apache.commons.pool.impl.GenericKeyedObjectPool;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
@@ -47,30 +49,54 @@ public class GraphiteWriterTests {
 			throw npe;
 		}
 	}
-
-	@Test
-	public void writeSingleResult() throws Exception {
-		// a lot of setup for not much of a test ...
-		Server server = Server.builder().setHost("host").setPort("123").build();
-		Query query = Query.builder().build();
-		Result result = new Result(System.currentTimeMillis(), "attributeName", "className", "objDomain", "classNameAlias", "typeName", ImmutableMap.of("key", (Object)1));
-
+	
+	private static GraphiteWriter getGraphiteWriter(OutputStream out, boolean useObjDomain) throws Exception {
 		GenericKeyedObjectPool<InetSocketAddress, Socket> pool = mock(GenericKeyedObjectPool.class);
 		Socket socket = mock(Socket.class);
 		when(pool.borrowObject(any(InetSocketAddress.class))).thenReturn(socket);
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		
 		when(socket.getOutputStream()).thenReturn(out);
 
 		GraphiteWriter writer = GraphiteWriter.builder()
 				.setHost("localhost")
 				.setPort(2003)
+				.setUseObjDomain(useObjDomain)
 				.build();
 		writer.setPool(pool);
+		
+		return writer;
+	}
+
+	@Test
+	public void writeSingleResult() throws Exception {
+		Server server = Server.builder().setHost("host").setPort("123").build();
+		Query query = Query.builder().build();
+		Result result = new Result(System.currentTimeMillis(), "attributeName", "className", "objDomain", "classNameAlias", "typeName", ImmutableMap.of("key", (Object)1));
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		GraphiteWriter writer = getGraphiteWriter(out, false);
 
 		writer.doWrite(server, query, of(result));
 
 		// check that Graphite format is respected
 		assertThat(out.toString()).startsWith("servers.host_123.classNameAlias.attributeName_key 1 ");
+	}
+	
+	@Test
+	public void useObjDomainWorks() throws Exception {
+		Server server = Server.builder().setHost("host").setPort("123").build();
+		Query query = Query.builder().build();
+		Result result = new Result(System.currentTimeMillis(), "attributeName", "className", "objDomain", null, "typeName", ImmutableMap.of("key", (Object)1));
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		
+		// Set useObjDomain to true
+		GraphiteWriter writer = getGraphiteWriter(out, true);
+
+		writer.doWrite(server, query, of(result));
+
+		// check that Graphite format is respected
+		assertThat(out.toString()).startsWith("servers.host_123.objDomain.attributeName_key 1 ");
 	}
 
 
