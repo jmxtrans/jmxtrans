2214c38e14dca216fa1ceb0a619022126d846b35
Conor Beverland <conorbev@users.noreply.github.com>
2015-04-17 18:19:15 -0700

    Whitespace

--- src/com/googlecode/jmxtrans/model/naming/KeyUtils.java
+++ src/com/googlecode/jmxtrans/model/naming/KeyUtils.java
@@ -76,14 +76,14 @@ public final class KeyUtils {
 
 	/**
 	 * Adds a key to the StringBuilder
-	 *
+	 * 
 	 * It uses in order of preference:
-	 *
+	 * 
 	 * 1. resultAlias if that was specified as part of the query
 	 * 2. The domain portion of the ObjectName in the query if useObjDomainAsKey is set to true
 	 * 3. else, the Class Name of the MBean. I.e. ClassName will be used by default if the
 	 * user doesn't specify anything special
-	 *
+	 * 
 	 * @param result
 	 * @param sb
 	 * @param useObjectDomain
