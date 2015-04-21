6d0316e9d3f4574e8e6a3c8a1e43e51405406e6a
Conor Beverland <conorbev@users.noreply.github.com>
2015-04-21 11:08:58 -0700

    Added some failing tests to catch the issue
    When using the typeNames property in your JMXTrans configuration if you
    specify a typeName that is empty within a particular JMX ObjectName then
    spurious extra delimeters (. or _ characters depending on your
    configuration) can appear in your output. These tests show how that can
    happen.

--- test/com/googlecode/jmxtrans/model/naming/StringUtilsTest.java
+++ test/com/googlecode/jmxtrans/model/naming/StringUtilsTest.java
@@ -10,30 +10,34 @@ public class StringUtilsTest {
 	public void testCleanupStr() {
 		assertEquals("addfber1241qdw!èé$", StringUtils.cleanupStr("addfber1241qdw!èé$"));
 		assertEquals("abcd_abcd", StringUtils.cleanupStr("abcd.abcd"));
-		assertEquals("abcd_abcd_", StringUtils.cleanupStr("abcd.abcd."));
+		assertEquals("abcd_abcd", StringUtils.cleanupStr("abcd.abcd."));
 		assertEquals("_abcd_abcd", StringUtils.cleanupStr(".abcd_abcd"));
 		assertEquals("abcd_abcd", StringUtils.cleanupStr("abcd/abcd"));
-		assertEquals("abcd_abcd_", StringUtils.cleanupStr("abcd/abcd/"));
+		assertEquals("abcd_abcd", StringUtils.cleanupStr("abcd/abcd/"));
 		assertEquals("_abcd_abcd", StringUtils.cleanupStr("/abcd_abcd"));
 		assertEquals("abcd_abcd", StringUtils.cleanupStr("abcd'_abcd'"));
 		assertEquals("_abcd_abcd", StringUtils.cleanupStr("/abcd\"_abcd\""));
 		assertEquals("_abcd_abcd", StringUtils.cleanupStr("/ab cd_abcd"));
 		assertEquals(null, StringUtils.cleanupStr(null));
 		assertEquals("", StringUtils.cleanupStr(""));
+		assertEquals("abcd", StringUtils.cleanupStr("\"abcd\".\"\""));
+		assertEquals("abcd", StringUtils.cleanupStr("\"abcd\"_\"\""));
 	}
 
 	@Test
 	public void testCleanupStrDottedKeysKept() {
 		assertEquals("addfber1241qdw!èé$", StringUtils.cleanupStr("addfber1241qdw!èé$", true));
 		assertEquals("abcd.abcd", StringUtils.cleanupStr("abcd.abcd", true));
-		assertEquals("abcd.abcd.", StringUtils.cleanupStr("abcd.abcd.", true));
+		assertEquals("abcd.abcd", StringUtils.cleanupStr("abcd.abcd.", true));
 		assertEquals(".abcd_abcd", StringUtils.cleanupStr(".abcd_abcd", true));
 		assertEquals("abcd_abcd", StringUtils.cleanupStr("abcd/abcd", true));
-		assertEquals("abcd_abcd_", StringUtils.cleanupStr("abcd/abcd/", true));
+		assertEquals("abcd_abcd", StringUtils.cleanupStr("abcd/abcd/", true));
 		assertEquals("_abcd_abcd", StringUtils.cleanupStr("/abcd_abcd", true));
 		assertEquals("abcd_abcd", StringUtils.cleanupStr("abcd'_abcd'", true));
 		assertEquals("_abcd_abcd", StringUtils.cleanupStr("/abcd\"_abcd\"", true));
 		assertEquals("_abcd_abcd", StringUtils.cleanupStr("/ab cd_abcd", true));
+		assertEquals("abcd", StringUtils.cleanupStr("\"abcd\".\"\""));
+		assertEquals("abcd", StringUtils.cleanupStr("\"abcd\"_\"\""));
 	}
 
 }
