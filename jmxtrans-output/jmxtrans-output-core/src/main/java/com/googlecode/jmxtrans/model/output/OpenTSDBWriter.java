fc977ae859d6019a63e2930f5eadcb9cfeb144a2
Ivan Gusev <igusev@C02KFC25DV33.local>
2015-05-31 23:25:05 -0700

    Switch OpenTSDB telnet writer to use GenericKeyedObjectPool and eliminate LifecycleExceptions

--- jmxtrans-output/jmxtrans-output-core/src/main/java/com/googlecode/jmxtrans/model/output/OpenTSDBWriter.java
+++ jmxtrans-output/jmxtrans-output-core/src/main/java/com/googlecode/jmxtrans/model/output/OpenTSDBWriter.java
@@ -4,20 +4,27 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.googlecode.jmxtrans.model.Query;
+import com.googlecode.jmxtrans.model.Result;
+import com.googlecode.jmxtrans.model.Server;
 import com.googlecode.jmxtrans.exceptions.LifecycleException;
+import org.apache.commons.pool.impl.GenericKeyedObjectPool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
+import javax.inject.Inject;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
+import java.net.ConnectException;
 import java.util.List;
 import java.util.Map;
 
 import static com.google.common.base.Charsets.UTF_8;
+import static com.googlecode.jmxtrans.model.PropertyResolver.resolveProps;
 
 /**
  * OpenTSDBWriter which directly sends
@@ -28,8 +35,8 @@ import static com.google.common.base.Charsets.UTF_8;
 public class OpenTSDBWriter extends OpenTSDBGenericWriter {
 	private static final Logger log = LoggerFactory.getLogger(OpenTSDBWriter.class);
 
-	protected Socket socket;
-	protected DataOutputStream out;
+	private GenericKeyedObjectPool<InetSocketAddress, Socket> pool;
+	private final InetSocketAddress address;
 
 	@JsonCreator
 	public OpenTSDBWriter(
@@ -46,6 +53,21 @@ public class OpenTSDBWriter extends OpenTSDBGenericWriter {
 			@JsonProperty("settings") Map<String, Object> settings) throws LifecycleException, UnknownHostException {
 		super(typeNames, booleanAsNumber, debugEnabled, host, port, tags, tagName, mergeTypeNamesTags, metricNamingExpression,
 				addHostnameTag, settings);
+		host = resolveProps(host);
+		if (host == null) {
+			host = (String) getSettings().get(HOST);
+		}
+		if (host == null) {
+			throw new NullPointerException("Host cannot be null.");
+		}
+		if (port == null) {
+			port = Settings.getIntegerSetting(getSettings(), PORT, null);
+		}
+		if (port == null) {
+			throw new NullPointerException("Port cannot be null.");
+		}
+		this.address = new InetSocketAddress(host, port);
+
 	}
 
 	/**
@@ -57,94 +79,45 @@ public class OpenTSDBWriter extends OpenTSDBGenericWriter {
 		return true;
 	}
 
-	/**
-	 * Prepare for sending metrics.
-	 */
-	@Override
-	protected void prepareSender() throws LifecycleException {
-
-		if (host == null || port == null) {
-			throw new LifecycleException("Host and port for " + this.getClass().getSimpleName() + " output can't be null");
-		}
-
-		try {
-			socket = new Socket(host, port);
-		} catch (UnknownHostException e) {
-			log.error("error opening socket to OpenTSDB", e);
-			throw new LifecycleException(e);
-		} catch (IOException e) {
-			log.error("error opening socket to OpenTSDB", e);
-			throw new LifecycleException(e);
-		}
-	}
-
-	/**
-	 * Shutdown the sender as it will no longer be used to send metrics.
-	 */
-	@Override
-	protected void shutdownSender() throws LifecycleException {
-		try {
-			socket.close();
-		} catch (IOException e) {
-			log.error("error closing socket to OpenTSDB", e);
-			throw new LifecycleException(e);
-		}
-	}
-
-	/**
-	 * Start the output for the results of a Query to OpenTSDB.
-	 */
-	@Override
-	protected void startOutput() throws IOException {
-		try {
-			this.out = new DataOutputStream(socket.getOutputStream());
-		} catch (IOException e) {
-			log.error("error getting the output stream", e);
-			throw e;
-		}
-	}
-
-	/**
-	 * Send a single metric to the server.
-	 *
-	 * @param metricLine - the line containing the metric name, value, and tags for a single metric; excludes the
-	 *                   "put" keyword expected by OpenTSDB and the trailing newline character.
-	 */
 	@Override
 	protected void sendOutput(String metricLine) throws IOException {
-		try {
-			
-			if (isDebugEnabled() || log.isDebugEnabled()) {
-				log.debug("Sending to server {}:{} line={}", host, port, metricLine);
-			}
-			
-			this.out.writeBytes("put " + metricLine + "\n");
-		} catch (IOException e) {
-			log.error("error writing result to the output stream", e);
-			throw e;
-		}
 	}
 
-	/**
-	 * Finish the output for a single Query, flushing all data to the server and logging the server's response.
-	 */
 	@Override
-	protected void finishOutput() throws IOException {
-		try {
-			this.out.flush();
-		} catch (IOException e) {
-			log.error("flush failed", e);
-			throw e;
+	public void internalWrite(Server server, Query query, ImmutableList<Result> results) throws Exception {
+	Socket socket = null;
+	PrintWriter writer = null;
+
+	try {
+		socket = pool.borrowObject(address);
+		writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), UTF_8), true);
+
+		for (Result result : results) {
+			log.debug("Query result: {}", result);
+			Map<String, Object> resultValues = result.getValues();
+			if (resultValues != null) {
+				for (String resultString : resultParser(result)) {
+					log.debug("OpenTSDB Message: {}", resultString);
+					writer.write("put " + resultString + "\n");
+				}
+			}
 		}
 
-		// Read and log the response from the server for diagnostic purposes.
-
-		InputStreamReader socketInputStream = new InputStreamReader(socket.getInputStream(), UTF_8);
-		BufferedReader bufferedSocketInputStream = new BufferedReader(socketInputStream);
-		String line;
-		while (socketInputStream.ready() && (line = bufferedSocketInputStream.readLine()) != null) {
-			log.warn("OpenTSDB says: " + line);
+	} catch (ConnectException e) {
+		log.error("Error while connecting to OpenTSDB");
+	} finally {
+		if (writer != null && writer.checkError()) {
+			log.error("Error writing to OpenTSDB, clearing OpenTSDB socket pool");
+			pool.invalidateObject(address, socket);
+		} else {
+			pool.returnObject(address, socket);
 		}
+	}
+	}
+
+	@Inject
+	public void setPool(GenericKeyedObjectPool<InetSocketAddress, Socket> pool) {
+		this.pool = pool;
 	}
 
 	public static Builder builder() {
