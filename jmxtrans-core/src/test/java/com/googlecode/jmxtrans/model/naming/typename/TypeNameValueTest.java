36cc34eadccee018decb0b763ba6a6afe658bf6b
Bartosz Marcinkowski <bm371613@gmail.com>
2015-10-08 15:58:57 +0200

    Replaced custom helper with Google's helper

--- jmxtrans-core/src/test/java/com/googlecode/jmxtrans/model/naming/typename/TypeNameValueTest.java
+++ jmxtrans-core/src/test/java/com/googlecode/jmxtrans/model/naming/typename/TypeNameValueTest.java
@@ -1,12 +1,10 @@
 package com.googlecode.jmxtrans.model.naming.typename;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Map;
-
-import static com.google.common.collect.Maps.newHashMap;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TypeNameValueTest {
@@ -71,36 +69,14 @@ public class TypeNameValueTest {
     public void testExtractMap() {
         assertThat(TypeNameValue.extractMap(null)).isEmpty();
         assertThat(TypeNameValue.extractMap("")).isEmpty();
-        assertThat(TypeNameValue.extractMap("x-key1-x")).isEqualTo(makeMap("x-key1-x", ""));
-        assertThat(TypeNameValue.extractMap("x-key1-x,x-key2-x")).isEqualTo(makeMap("x-key1-x", "", "x-key2-x", ""));
-        assertThat(TypeNameValue.extractMap("x-key1-x=x-value1-x")).isEqualTo(makeMap("x-key1-x", "x-value1-x"));
-
+        assertThat(TypeNameValue.extractMap("x-key1-x")).isEqualTo(ImmutableMap.of("x-key1-x", ""));
+        assertThat(TypeNameValue.extractMap("x-key1-x,x-key2-x")).isEqualTo(ImmutableMap.of("x-key1-x", "", "x-key2-x", ""));
+        assertThat(TypeNameValue.extractMap("x-key1-x=x-value1-x")).isEqualTo(ImmutableMap.of("x-key1-x", "x-value1-x"));
         assertThat(TypeNameValue.extractMap("x-key1-x=x-value1-x,y-key2-y=y-value2-y"))
-                .isEqualTo(makeMap("x-key1-x", "x-value1-x", "y-key2-y", "y-value2-y"));
+                .isEqualTo(ImmutableMap.of("x-key1-x", "x-value1-x", "y-key2-y", "y-value2-y"));
         assertThat(TypeNameValue.extractMap("x-key1-x=x-value1-x,y-key2-y=y-value2-y,z-key3-z=z-value3-z"))
-                .isEqualTo(makeMap("x-key1-x", "x-value1-x", "y-key2-y", "y-value2-y", "z-key3-z", "z-value3-z"));
+                .isEqualTo(ImmutableMap.of("x-key1-x", "x-value1-x", "y-key2-y", "y-value2-y", "z-key3-z", "z-value3-z"));
         assertThat(TypeNameValue.extractMap("x-key1-x=x-value1-x,y-key2-y,yy-key2.5-yy=a=1,z-key3-z=z-value3-z"))
-                .isEqualTo(makeMap("x-key1-x", "x-value1-x", "y-key2-y", "", "yy-key2.5-yy", "a=1", "z-key3-z", "z-value3-z"));
-    }
-
-    /** Convenience method for creating a Map for comparison. */
-    private Map<String, String> makeMap(String... keysAndValues) {
-        Map<String, String> result;
-        int cur;
-
-        result = newHashMap();
-
-        cur = 0;
-        while (cur < keysAndValues.length) {
-            if (cur < keysAndValues.length - 1) {
-                result.put(keysAndValues[cur], keysAndValues[cur + 1]);
-                cur += 2;
-            } else {
-                result.put(keysAndValues[cur], "");
-                cur++;
-            }
-        }
-
-        return result;
+                .isEqualTo(ImmutableMap.of("x-key1-x", "x-value1-x", "y-key2-y", "", "yy-key2.5-yy", "a=1", "z-key3-z", "z-value3-z"));
     }
 }
