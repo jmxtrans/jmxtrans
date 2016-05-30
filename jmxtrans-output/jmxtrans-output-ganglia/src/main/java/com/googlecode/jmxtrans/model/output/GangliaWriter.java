fcb8668ffd49bf873bbfb9b52389fb444b8b6f9e
Guillaume Lederrey <glederrey@wikimedia.org>
2016-05-30 09:08:12 +0200

    Close GMetric properly when sending metrics to Ganglia.
    Fixes #466

--- jmxtrans-output/jmxtrans-output-ganglia/src/main/java/com/googlecode/jmxtrans/model/output/GangliaWriter.java
+++ jmxtrans-output/jmxtrans-output-ganglia/src/main/java/com/googlecode/jmxtrans/model/output/GangliaWriter.java
@@ -199,8 +199,9 @@ public class GangliaWriter extends BaseOutputWriter {
 
 				GMetricType dataType = getType(resultValue.getValue());
 				log.debug("Sending Ganglia metric {}={} [type={}]", name, transformedValue, dataType);
-				new GMetric(host, port, addressingMode, ttl, v31, null, spoofedHostName)
-						.announce(name, transformedValue.toString(), dataType, units, slope, tmax, dmax, groupName);
+				try (GMetric metric = new GMetric(host, port, addressingMode, ttl, v31, null, spoofedHostName)) {
+					metric.announce(name, transformedValue.toString(), dataType, units, slope, tmax, dmax, groupName);
+				}
 			}
 		}
 	}
