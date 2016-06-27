b90a8c7f09bd17cf65be30847afc781dbf08f051
berichard <b.richard@paris-turf.com>
2016-06-27 17:18:54 +0200

    More indentation issue

--- jmxtrans-output/jmxtrans-output-influxdb/src/main/java/com/googlecode/jmxtrans/model/output/InfluxDbWriter.java
+++ jmxtrans-output/jmxtrans-output-influxdb/src/main/java/com/googlecode/jmxtrans/model/output/InfluxDbWriter.java
@@ -1,17 +1,17 @@
 /**
  * The MIT License
  * Copyright © 2010 JmxTrans team
- * <p>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * <p>
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * <p>
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -57,16 +57,11 @@ public class InfluxDbWriter extends OutputWriterAdapter {
 
 	public static final String TAG_HOSTNAME = "hostname";
 
-	@Nonnull
-	private final InfluxDB influxDB;
-	@Nonnull
-	private final String database;
-	@Nonnull
-	private final ConsistencyLevel writeConsistency;
-	@Nonnull
-	private final String retentionPolicy;
-	@Nonnull
-	private final ImmutableMap<String, String> tags;
+	@Nonnull private final InfluxDB influxDB;
+	@Nonnull private final String database;
+	@Nonnull private final ConsistencyLevel writeConsistency;
+	@Nonnull private final String retentionPolicy;
+	@Nonnull private final ImmutableMap<String,String> tags;
 
 	/**
 	 * The {@link ImmutableSet} of {@link ResultAttribute} attributes of
@@ -81,7 +76,7 @@ public class InfluxDbWriter extends OutputWriterAdapter {
 			@Nonnull String database,
 			@Nonnull ConsistencyLevel writeConsistency,
 			@Nonnull String retentionPolicy,
-			@Nonnull ImmutableMap<String, String> tags,
+			@Nonnull ImmutableMap<String,String> tags,
 			@Nonnull ImmutableSet<ResultAttribute> resultAttributesToWriteAsTags,
 			boolean createDatabase) {
 		this.database = database;
@@ -157,8 +152,8 @@ public class InfluxDbWriter extends OutputWriterAdapter {
 		BatchPoints.Builder batchPointsBuilder = BatchPoints.database(database).retentionPolicy(retentionPolicy)
 				.tag(TAG_HOSTNAME, server.getSource());
 
-		for (Map.Entry<String, String> tag : tags.entrySet()) {
-			batchPointsBuilder.tag(tag.getKey(), tag.getValue());
+		for(Map.Entry<String,String> tag : tags.entrySet()) {
+			batchPointsBuilder.tag(tag.getKey(),tag.getValue());
 		}
 		BatchPoints batchPoints = batchPointsBuilder.consistency(writeConsistency).build();
 		for (Result result : results) {
