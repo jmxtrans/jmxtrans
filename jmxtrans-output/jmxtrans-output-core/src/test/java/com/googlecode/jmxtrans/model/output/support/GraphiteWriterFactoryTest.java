ab77b8276c32d051582583920ceef2e40485f504
Christian Nguyen Van Than <christian.nguyenvanthan@gmail.com>
2017-04-14 10:18:46 +0200

    add udp support for graphite writer factory

--- jmxtrans-output/jmxtrans-output-core/src/test/java/com/googlecode/jmxtrans/model/output/support/GraphiteWriterFactoryTest.java
+++ jmxtrans-output/jmxtrans-output-core/src/test/java/com/googlecode/jmxtrans/model/output/support/GraphiteWriterFactoryTest.java
@@ -23,7 +23,6 @@
 package com.googlecode.jmxtrans.model.output.support;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.io.Closer;
 import com.google.inject.Injector;
 import com.googlecode.jmxtrans.ConfigurationParser;
 import com.googlecode.jmxtrans.cli.JmxTransConfiguration;
@@ -32,21 +31,22 @@ import com.googlecode.jmxtrans.guice.JmxTransModule;
 import com.googlecode.jmxtrans.model.OutputWriter;
 import com.googlecode.jmxtrans.model.Query;
 import com.googlecode.jmxtrans.model.Server;
-import com.googlecode.jmxtrans.model.output.GraphiteWriter;
-import com.googlecode.jmxtrans.model.output.GraphiteWriter2;
+import com.googlecode.jmxtrans.model.output.support.pool.DatagramChannelAllocator;
+import com.googlecode.jmxtrans.model.output.support.pool.RetryingAllocator;
 import com.googlecode.jmxtrans.test.IntegrationTest;
 import com.googlecode.jmxtrans.test.RequiresIO;
-import com.googlecode.jmxtrans.test.ResetableSystemProperty;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import stormpot.BlazePool;
+import stormpot.LifecycledPool;
 
 import java.io.File;
-import java.io.IOException;
+import java.lang.reflect.Field;
 import java.net.URISyntaxException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
 
 @Category({IntegrationTest.class, RequiresIO.class})
 public class GraphiteWriterFactoryTest {
@@ -84,6 +84,98 @@ public class GraphiteWriterFactoryTest {
 
 		assertThat(writerPoolOutputWriter.getSocketTimeoutMs()).isEqualTo(1000);
 		assertThat(writerPoolOutputWriter.getPoolClaimTimeout().getTimeout()).isEqualTo(2);
+	}
+
+	@Test
+	public void use_tcp_protocol_by_default() throws LifecycleException, URISyntaxException {
+		ImmutableList<Server> servers = configurationParser.parseServers(ImmutableList.of(file("/graphite-writer-factory-example2.json")), false);
+
+		assertThat(servers).hasSize(1);
+		Server server = servers.get(0);
+
+		assertThat(server.getQueries()).hasSize(1);
+		Query query = server.getQueries().iterator().next();
+
+		assertThat(query.getOutputWriterInstances()).hasSize(1);
+		OutputWriter outputWriter = query.getOutputWriterInstances().iterator().next();
+
+		assertThat(outputWriter).isInstanceOf(ResultTransformerOutputWriter.class);
+
+		ResultTransformerOutputWriter resultTransformerOutputWriter = (ResultTransformerOutputWriter) outputWriter;
+		OutputWriter target = resultTransformerOutputWriter.getTarget();
+
+		assertThat(target).isInstanceOf(WriterPoolOutputWriter.class);
+
+		LifecycledPool writerPool = ((WriterPoolOutputWriter) target).getWriterPool();
+
+		assertThat(writerPool).isInstanceOf(BlazePool.class);
+
+		BlazePool blazePool = (BlazePool) writerPool;
+
+		// using TcpOutputWriterBuilder
+		try {
+			// first level allocator
+			Field allocator = blazePool.getClass().getDeclaredField("allocator");
+			allocator.setAccessible(true);
+			Object insideAllocator = allocator.get(blazePool);
+			// second level allocator
+			Field allocatorLv2 = insideAllocator.getClass().getDeclaredField("allocator");
+			allocatorLv2.setAccessible(true);
+			Object level2Allocator = allocatorLv2.get(insideAllocator);
+			// third level
+			Field allocatorLv3 = level2Allocator.getClass().getDeclaredField("allocator");
+			allocatorLv3.setAccessible(true);
+			Object level3Allocator = allocatorLv3.get(level2Allocator);
+			assertThat(level3Allocator).isInstanceOf(RetryingAllocator.class);
+		} catch (IllegalAccessException | NoSuchFieldException e) {
+			fail();
+		}
+	}
+
+	@Test
+	public void writer_using_udp_protocol() throws LifecycleException, URISyntaxException {
+		ImmutableList<Server> servers = configurationParser.parseServers(ImmutableList.of(file("/graphite-writer-factory-example-with-udp.json")), false);
+
+		assertThat(servers).hasSize(1);
+		Server server = servers.get(0);
+
+		assertThat(server.getQueries()).hasSize(1);
+		Query query = server.getQueries().iterator().next();
+
+		assertThat(query.getOutputWriterInstances()).hasSize(1);
+		OutputWriter outputWriter = query.getOutputWriterInstances().iterator().next();
+
+		assertThat(outputWriter).isInstanceOf(ResultTransformerOutputWriter.class);
+
+		ResultTransformerOutputWriter resultTransformerOutputWriter = (ResultTransformerOutputWriter) outputWriter;
+		OutputWriter target = resultTransformerOutputWriter.getTarget();
+
+		assertThat(target).isInstanceOf(WriterPoolOutputWriter.class);
+
+		LifecycledPool writerPool = ((WriterPoolOutputWriter) target).getWriterPool();
+
+		assertThat(writerPool).isInstanceOf(BlazePool.class);
+
+		BlazePool blazePool = (BlazePool) writerPool;
+
+		// using UdpOutputWriterBuilder
+		try {
+			// first level allocator
+			Field allocator = blazePool.getClass().getDeclaredField("allocator");
+			allocator.setAccessible(true);
+			Object insideAllocator = allocator.get(blazePool);
+			// second level allocator
+			Field allocatorLv2 = insideAllocator.getClass().getDeclaredField("allocator");
+			allocatorLv2.setAccessible(true);
+			Object level2Allocator = allocatorLv2.get(insideAllocator);
+			// third level
+			Field allocatorLv3 = level2Allocator.getClass().getDeclaredField("allocator");
+			allocatorLv3.setAccessible(true);
+			Object level3Allocator = allocatorLv3.get(level2Allocator);
+			assertThat(level3Allocator).isInstanceOf(DatagramChannelAllocator.class);
+		} catch (IllegalAccessException | NoSuchFieldException e) {
+			fail();
+		}
 	}
 
 	private File file(String filename) throws URISyntaxException {
