52a090f22ebae4747f8fd01026b4b5b9411ae94a
Dennis Ploeger <dennis.ploeger@kps.com>
2017-07-17 12:30:40 +0200

    Fixes as requested

--- jmxtrans-output/jmxtrans-output-gelf/src/main/java/com/googlecode/jmxtrans/model/output/gelf/GelfWriter.java
+++ jmxtrans-output/jmxtrans-output-gelf/src/main/java/com/googlecode/jmxtrans/model/output/gelf/GelfWriter.java
@@ -1,17 +1,17 @@
 /**
  * The MIT License
  * Copyright © 2010 JmxTrans team
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,7 +22,6 @@
  */
 package com.googlecode.jmxtrans.model.output.gelf;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.ImmutableList;
 import com.googlecode.jmxtrans.model.Query;
 import com.googlecode.jmxtrans.model.Result;
@@ -31,14 +30,11 @@ import com.googlecode.jmxtrans.model.ValidationException;
 import com.googlecode.jmxtrans.model.naming.KeyUtils;
 import com.googlecode.jmxtrans.model.output.BaseOutputWriter;
 import org.apache.commons.lang.StringUtils;
-import org.graylog2.gelfclient.GelfConfiguration;
 import org.graylog2.gelfclient.GelfMessageBuilder;
-import org.graylog2.gelfclient.GelfTransports;
 import org.graylog2.gelfclient.transport.GelfTransport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -48,104 +44,19 @@ public class GelfWriter extends BaseOutputWriter {
 	private static final Logger log = LoggerFactory.getLogger(GelfWriter.class);
 
 	private final Map<String, Object> additionalFields;
-	private final GelfConfiguration gelfConfiguration;
-
-	private boolean useMock = false;
+	private final GelfTransport gelfTransport;
 
 	public GelfWriter(
-		@JsonProperty("typeNames") final ImmutableList<String> typeNames,
-		@JsonProperty("booleanAsNumber") final boolean booleanAsNumber,
-		@JsonProperty("debugEnabled") final Boolean debugEnabled,
-		@JsonProperty("settings") final Map<String, Object> settings,
-		@JsonProperty("host") final String host,
-		@JsonProperty("port") Integer port,
-		@JsonProperty("additionalFields") final Map<String, Object> additionalFields,
-		@JsonProperty("transport") final String transport,
-		@JsonProperty("queueSize") final Integer queueSize,
-		@JsonProperty("connectTimeout") final Integer connectTimeout,
-		@JsonProperty("reconnectDelay") final Integer reconnectDelay,
-		@JsonProperty("tcpNoDelay") final Boolean tcpNoDelay,
-		@JsonProperty("sendBufferSize") final Integer sendBufferSize,
-		@JsonProperty("tlsEnabled") final Boolean tlsEnabled,
-		@JsonProperty("tlsTrustCertChainFile") final String tlsTrustCertChainFile,
-		@JsonProperty("tlsCertVerificationEnabled") final Boolean tlsCertVerificationEnabled,
-		@JsonProperty("tcpKeepAlive") final Boolean tcpKeepAlive,
-		@JsonProperty("maxInflightSends") final Integer maxInflightSends
-	) {
-		super(typeNames, booleanAsNumber, debugEnabled, settings);
+		ImmutableList<String> typeNames,
+		boolean booleanAsNumber,
+		Boolean debugEnabled,
+		Map<String, Object> settings,
+		Map<String, Object> additionalFields,
+	    GelfTransport gelfTransport)
+	{
+		super(typeNames,booleanAsNumber, debugEnabled, settings);
 		this.additionalFields = additionalFields;
-
-		if (host == null) {
-			throw new NullPointerException("Host can not be null");
-		}
-
-		if (port != null) {
-			this.gelfConfiguration = new GelfConfiguration(host, port);
-		} else {
-			this.gelfConfiguration = new GelfConfiguration(host);
-		}
-
-		if (transport != null) {
-			final GelfTransports gelfTransports;
-
-			switch (transport.toUpperCase()) {
-				case "TCP": gelfTransports = GelfTransports.TCP;
-					break;
-				case "UDP": gelfTransports = GelfTransports.UDP;
-					break;
-				case "TEST": useMock = true;
-				default: gelfTransports = GelfTransports.TCP;
-			}
-
-			gelfConfiguration.transport(gelfTransports);
-		}
-
-		if (queueSize != null) {
-			gelfConfiguration.queueSize(queueSize);
-		}
-
-		if (connectTimeout != null) {
-			gelfConfiguration.connectTimeout(connectTimeout);
-		}
-
-		if (reconnectDelay != null) {
-			gelfConfiguration.reconnectDelay(reconnectDelay);
-		}
-
-		if (tcpNoDelay != null) {
-			gelfConfiguration.tcpNoDelay(tcpNoDelay);
-		}
-
-		if (sendBufferSize != null) {
-			gelfConfiguration.sendBufferSize(sendBufferSize);
-		}
-
-		if (tlsEnabled != null) {
-			if (tlsEnabled) {
-				gelfConfiguration.enableTls();
-			} else {
-				gelfConfiguration.disableTls();
-			}
-		}
-
-		if (tlsCertVerificationEnabled != null) {
-			if (tlsCertVerificationEnabled) {
-				gelfConfiguration.enableTlsCertVerification();
-			} else {
-				gelfConfiguration.disableTlsCertVerification();
-			}
-		}
-
-		if (tlsTrustCertChainFile != null) {
-			gelfConfiguration.tlsTrustCertChainFile(new File(tlsTrustCertChainFile));
-		}
-
-		if (maxInflightSends != null) {
-			gelfConfiguration.maxInflightSends(maxInflightSends);
-		}
-
-		log.debug("Created gelf configuration: %s", gelfConfiguration.toString());
-
+		this.gelfTransport = gelfTransport;
 	}
 
 	@Override
@@ -163,14 +74,6 @@ public class GelfWriter extends BaseOutputWriter {
 	{
 		final List<String> typeNames = this.getTypeNames();
 
-		final GelfTransport transport;
-
-		if (useMock) {
-			transport = new MockGelfTransport();
-		} else {
-			transport = GelfTransports.create(this.gelfConfiguration);
-		}
-
 		final GelfMessageBuilder messageBuilder = new GelfMessageBuilder(
 			"",
 			server.getHost()
@@ -180,8 +83,11 @@ public class GelfWriter extends BaseOutputWriter {
 
 		for (final Result result : results) {
 			log.debug("Query result: [{}]", result);
-			for (final Map.Entry<String, Object> values: result.getValues().entrySet()) {
-				final String key = KeyUtils.getKeyString(query, result, values, typeNames);
+			for (final Map.Entry<String, Object> values : result.getValues()
+				.entrySet()) {
+				final String
+					key =
+					KeyUtils.getKeyString(query, result, values, typeNames);
 
 				messages.add(
 					String.format(
@@ -209,12 +115,11 @@ public class GelfWriter extends BaseOutputWriter {
 		messageBuilder.fullMessage(message);
 
 		log.debug(
-			String.format(
-				"Sending GELF message: %s",
-				messageBuilder.build().toString()
-			)
+			"Sending GELF message: {}",
+			messageBuilder.build().toString()
 		);
 
-		transport.send(messageBuilder.build());
+		this.gelfTransport.send(messageBuilder.build());
 	}
+
 }
--- jmxtrans-output/jmxtrans-output-gelf/src/test/java/com/googlecode/jmxtrans/model/output/gelf/GelfWriterTests.java
+++ jmxtrans-output/jmxtrans-output-gelf/src/test/java/com/googlecode/jmxtrans/model/output/gelf/GelfWriterTests.java
@@ -1,17 +1,17 @@
 /**
  * The MIT License
  * Copyright © 2010 JmxTrans team
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,17 +23,29 @@
 package com.googlecode.jmxtrans.model.output.gelf;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import com.googlecode.jmxtrans.model.Result;
-import org.junit.Assert;
+import com.googlecode.jmxtrans.model.ResultFixtures;
+import com.googlecode.jmxtrans.model.ValidationException;
+import org.graylog2.gelfclient.GelfMessage;
+import org.graylog2.gelfclient.transport.GelfTcpTransport;
+import org.graylog2.gelfclient.transport.GelfTransport;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentMatcher;
+import org.mockito.Mockito;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static com.googlecode.jmxtrans.model.QueryFixtures.dummyQuery;
 import static com.googlecode.jmxtrans.model.ServerFixtures.dummyServer;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class GelfWriterTests {
 
@@ -41,58 +53,285 @@ public class GelfWriterTests {
 
 	@Before
 	public void setUp() throws Exception {
-		this.result = new Result(1, "attributeName", "className", "objDomain", "classNameAlias", "typeName", ImmutableMap
-			.of("key", (Object)1));
-
+		this.result = ResultFixtures.numericResult();
 	}
 
 	@Test
 	public void testSendGelfMessage() throws Exception {
 		final ImmutableList<String> typenames = ImmutableList.of();
-		final Map<String,Object> settings = new HashMap<String,Object>();
+		final Map<String, Object> settings = new HashMap<>();
 
 		final Map<String, Object> additionalFields = new HashMap<>();
 
 		additionalFields.put("test", "test");
 
+		final GelfTransport gelfTransport = Mockito.mock(GelfTcpTransport.class);
+
+		doNothing().when(gelfTransport).send(
+			argThat(
+				new ArgumentMatcher<GelfMessage>() {
+					@Override
+					public boolean matches(final Object o) {
+						final GelfMessage message = (GelfMessage) o;
+						assertThat(message.getAdditionalFields())
+							.containsKey("test")
+							.as("Additional fields were not checked");
+
+						assertThat(message.getFullMessage())
+							.isNotEmpty()
+							.as("Invalid full message");
+						assertThat(message.getMessage())
+							.isNotEmpty()
+							.as("Invalid message");
+						return true;
+					}
+				}
+			)
+		);
+
 		final GelfWriter gelfWriter;
 
+		gelfWriter = new GelfWriter(
+			typenames,
+			true,
+			true,
+			settings,
+			additionalFields,
+			gelfTransport
+		);
+		gelfWriter.start();
+		gelfWriter.doWrite(dummyServer(),
+			dummyQuery(),
+			ImmutableList.of(result));
+
+		verify(
+			gelfTransport,
+			times(1)
+		).send(isA(GelfMessage.class));
+	}
+
+	@Test
+	public void testTcpDefaultsWriterFactory() throws ValidationException {
+
+		final ImmutableList<String> typenames = ImmutableList.of();
+		final Map<String, Object> settings = new HashMap<>();
+
+		final Map<String, Object> additionalFields = new HashMap<>();
+
+		additionalFields.put("test", "test");
+
+		final GelfWriterFactory gelfWriterFactory = new GelfWriterFactory(
+			typenames,
+			true,
+			true,
+			settings,
+			"test",
+			null,
+			additionalFields,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null
+		);
+
+		final GelfWriter gelfWriter = gelfWriterFactory.create();
+		gelfWriter.validateSetup(dummyServer(), dummyQuery());
+
+		assertThat(gelfWriter).isNotNull().as("Didn't get a writer back.");
+	}
+
+	@Test
+	public void testUdpDefaultsWriterFactory() throws ValidationException {
+
+		final ImmutableList<String> typenames = ImmutableList.of();
+		final Map<String, Object> settings = new HashMap<>();
+
+		final Map<String, Object> additionalFields = new HashMap<>();
+
+		additionalFields.put("test", "test");
+
+		final GelfWriterFactory gelfWriterFactory = new GelfWriterFactory(
+			typenames,
+			true,
+			true,
+			settings,
+			"test",
+			null,
+			additionalFields,
+			"udp",
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null
+		);
+
+		final GelfWriter gelfWriter = gelfWriterFactory.create();
+		gelfWriter.validateSetup(dummyServer(), dummyQuery());
+
+		assertThat(gelfWriter).isNotNull().as("Didn't get a writer back.");
+	}
+
+	@Test
+	public void testNonDefaultsWriterFactory() throws ValidationException {
+
+		final ImmutableList<String> typenames = ImmutableList.of();
+		final Map<String, Object> settings = new HashMap<>();
+
+		final Map<String, Object> additionalFields = new HashMap<>();
+
+		additionalFields.put("test", "test");
+
+		final GelfWriterFactory gelfWriterFactory = new GelfWriterFactory(
+			typenames,
+			true,
+			true,
+			settings,
+			"test",
+			12202,
+			additionalFields,
+			"udp",
+			10,
+			10,
+			10,
+			true,
+			10,
+			true,
+			"/somefile",
+			true,
+			true,
+			10
+		);
+
+		final GelfWriter gelfWriter = gelfWriterFactory.create();
+		gelfWriter.validateSetup(dummyServer(), dummyQuery());
+
+		assertThat(gelfWriter).isNotNull().as("Didn't get a writer back.");
+	}
+
+	@Test
+	public void testNonDefaultsFalseWriterFactory() throws ValidationException {
+
+		final ImmutableList<String> typenames = ImmutableList.of();
+		final Map<String, Object> settings = new HashMap<>();
+
+		final Map<String, Object> additionalFields = new HashMap<>();
+
+		additionalFields.put("test", "test");
+
+		final GelfWriterFactory gelfWriterFactory = new GelfWriterFactory(
+			typenames,
+			true,
+			true,
+			settings,
+			"test",
+			null,
+			additionalFields,
+			"tcp",
+			10,
+			10,
+			10,
+			false,
+			10,
+			false,
+			"/somefile",
+			false,
+			false,
+			10
+		);
+
+		final GelfWriter gelfWriter = gelfWriterFactory.create();
+		gelfWriter.validateSetup(dummyServer(), dummyQuery());
+
+		assertThat(gelfWriter).isNotNull().as("Didn't get a writer back.");
+	}
+	@Test
+	public void testIgnoreWrongTransport() throws ValidationException {
+
+		final ImmutableList<String> typenames = ImmutableList.of();
+		final Map<String, Object> settings = new HashMap<>();
+
+		final Map<String, Object> additionalFields = new HashMap<>();
+
+		additionalFields.put("test", "test");
+
+		final GelfWriterFactory gelfWriterFactory = new GelfWriterFactory(
+			typenames,
+			true,
+			true,
+			settings,
+			"test",
+			null,
+			additionalFields,
+			"bogus,
+			10,
+			10,
+			10,
+			false,
+			10,
+			false,
+			"/somefile",
+			false,
+			false,
+			10
+		);
+
+		final GelfWriter gelfWriter = gelfWriterFactory.create();
+		gelfWriter.validateSetup(dummyServer(), dummyQuery());
+
+		assertThat(gelfWriter).isNotNull().as("Didn't get a writer back.");
+	}
+
+	@Test
+	public void testHostNotSpecified() throws ValidationException {
+
+		final ImmutableList<String> typenames = ImmutableList.of();
+		final Map<String, Object> settings = new HashMap<>();
+
+		final Map<String, Object> additionalFields = new HashMap<>();
+
+		additionalFields.put("test", "test");
+
 		try {
-			gelfWriter = new GelfWriter(
+			final GelfWriterFactory gelfWriterFactory = new GelfWriterFactory(
 				typenames,
 				true,
 				true,
 				settings,
-				"gelf.example.com",
+				null,
 				null,
 				additionalFields,
-				"TEST",
-				null,
-				null,
-				null,
-				null,
-				null,
-				null,
-				null,
-				null,
-				null,
-				null
+				"udp",
+				10,
+				10,
+				10,
+				true,
+				10,
+				true,
+				"/somefile",
+				true,
+				true,
+				10
 			);
 		} catch (final NullPointerException e) {
-			throw new RuntimeException("Unexpected failure to create gelfwriter for test", e);
+			assertThat(e).isNotNull();
+			return;
 		}
-		gelfWriter.start();
-		gelfWriter.doWrite(dummyServer(), dummyQuery(), ImmutableList.<Result>of(result));
-		Assert.assertTrue(
-			"Transport hasn't been called",
-			MockGelfTransport.calls.size() > 0
-		);
 
-		Assert.assertEquals(
-			"Additional fields not set",
-			"test",
-			MockGelfTransport.calls.get(0).getAdditionalFields().get("test")
-		);
+		fail("No error received");
+
 	}
 
 }
