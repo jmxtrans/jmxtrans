d769aa04701e547262173cb866143920d2f5ac52
Dennis Ploeger <dennis.ploeger@kps.com>
2017-07-17 15:50:55 +0200

    Fixes as requested

--- jmxtrans-output/jmxtrans-output-gelf/src/test/java/com/googlecode/jmxtrans/model/output/gelf/GelfWriterTests.java
+++ jmxtrans-output/jmxtrans-output-gelf/src/test/java/com/googlecode/jmxtrans/model/output/gelf/GelfWriterTests.java
@@ -1,17 +1,17 @@
 /**
  * The MIT License
  * Copyright © 2010 JmxTrans team
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,9 +28,13 @@ import com.googlecode.jmxtrans.model.ResultFixtures;
 import org.graylog2.gelfclient.GelfMessage;
 import org.graylog2.gelfclient.transport.GelfTcpTransport;
 import org.graylog2.gelfclient.transport.GelfTransport;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatcher;
+import org.mockito.Captor;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -46,33 +50,40 @@ import static org.mockito.Mockito.verify;
 
 public class GelfWriterTests {
 
+	@Captor
+	ArgumentCaptor<GelfMessage> messageArgumentCaptor;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+
 	@Test
 	public void testSendGelfMessage() throws Exception {
 		final ImmutableList<String> typenames = ImmutableList.of();
 		final Map<String, Object> settings = new HashMap<>();
 
-		final GelfTransport gelfTransport = Mockito.mock(GelfTcpTransport.class);
+		final GelfTransport
+			gelfTransport =
+			Mockito.mock(GelfTcpTransport.class);
 
 		doNothing().when(gelfTransport).send(
-			argThat(
-				new ArgumentMatcher<GelfMessage>() {
-					@Override
-					public boolean matches(final Object o) {
-						final GelfMessage message = (GelfMessage) o;
-						assertThat(message.getAdditionalFields())
-							.containsKey("test")
-							.as("Additional fields were not checked");
-						assertThat(message.getFullMessage())
-							.isNotEmpty()
-							.as("Invalid full message");
-						assertThat(message.getMessage())
-							.isNotEmpty()
-							.as("Invalid message");
-						return true;
-					}
-				}
-			)
+			messageArgumentCaptor.capture()
 		);
+
+		GelfMessage gelfMessage = messageArgumentCaptor.getValue();
+
+		assertThat(gelfMessage.getAdditionalFields())
+			.containsEntry("test", "test")
+			.as("Additional fields were not checked");
+
+		assertThat(gelfMessage.getFullMessage())
+			.isNotEmpty()
+			.as("Invalid full message");
+
+		assertThat(gelfMessage.getMessage())
+			.isNotEmpty()
+			.as("Invalid message");
 
 		final GelfWriter gelfWriter;
 
