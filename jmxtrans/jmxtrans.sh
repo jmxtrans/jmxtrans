ad98a6542b494089bc7e973bcbf3e0c9bcdd167d
Gérald Quintana <gerald.quintana@gmail.com>
2018-05-21 22:05:44 +0200

    Split jmxtrans.sh

--- jmxtrans/jmxtrans.sh
+++ jmxtrans/jmxtrans.sh
@@ -22,8 +22,8 @@
 # THE SOFTWARE.
 #
 
-
-FILENAME=$2
+SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE[0]} ) && pwd )
+RUN_SCRIPT="${SCRIPT_DIR}/jmxtrans-run.sh $2"
 
 # If CONF_FILE not defined but /etc/sysconfig/jmxtrans found, use it (servicectl/initd)
 if [ -z "$CONF_FILE" ]; then
@@ -32,53 +32,16 @@ if [ -z "$CONF_FILE" ]; then
     fi
 fi
 
-# Specify the commonly used configuration options below in a config file.
-CONF_FILE=${CONF_FILE:-"jmxtrans.conf"}
-if [ -e "$CONF_FILE" ]; then
-    . "$CONF_FILE"
-fi
-
-JAVA_HOME=${JAVA_HOME:-"/usr"}
-LOG_DIR=${LOG_DIR:-"."}
 STDOUT_FILE=${STDOUT_FILE:-"/dev/null"}
 
-JAR_FILE=${JAR_FILE:-"lib/jmxtrans-all.jar"}
-ADDITIONAL_JARS=${ADDITIONAL_JARS:-""}
-JSON_DIR=${JSON_DIR:-"."}
-SECONDS_BETWEEN_RUNS=${SECONDS_BETWEEN_RUNS:-"60"}
 HARDKILL_THRESHOLD=${HARDKILL_THRESHOLD:-60}
 
+JAVA_HOME=${JAVA_HOME:-"/usr"}
 JPS=${JPS:-"${JAVA_HOME}/bin/jps -l"}
 USE_JPS=${USE_JPS:-"true"}
 JAVA=${JAVA:-"${JAVA_HOME}/bin/java"}
-CHECK_JAVA=${CHECK_JAVA:-"true"}
 PSJAVA=${PSJAVA:-"ps aux | grep [j]ava"}
 PSCMD="$JPS | grep -i jmxtrans | awk '{ print \$1 };'"
-JAVA_OPTS=${JAVA_OPTS:-"-Djava.awt.headless=true -Djava.net.preferIPv4Stack=true"}
-HEAP_SIZE=${HEAP_SIZE:-"512"}
-PERM_SIZE=${PERM_SIZE:-"384"}
-MAX_PERM_SIZE=${MAX_PERM_SIZE:-"384"}
-JMX_PORT=${JMX_PORT:-"2101"}
-LOG_LEVEL=${LOG_LEVEL:-"debug"}
-CONTINUE_ON_ERROR=${CONTINUE_ON_ERROR:-"false"}
-JMXTRANS_OPTS="$JMXTRANS_OPTS -Djmxtrans.log.level=${LOG_LEVEL} -Djmxtrans.log.dir=$LOG_DIR"
-
-
-MONITOR_OPTS=${MONITOR_OPTS:-"-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=${JMX_PORT}"}
-GC_OPTS=${GC_OPTS:-"-Xms${HEAP_SIZE}m -Xmx${HEAP_SIZE}m -XX:PermSize=${PERM_SIZE}m -XX:MaxPermSize=${MAX_PERM_SIZE}m"}
-SSL_OPTS=${SSL_OPTS:-""}
-if [[ "${SSL_TRUSTSTORE}" != "" ]]; then
-    SSL_OPTS="${SSL_OPTS} -Djavax.net.ssl.trustStore=${SSL_TRUSTSTORE}"
-    if [[ "${SSL_TRUSTSTORE_PASSWORD}" != "" ]]; then
-        SSL_OPTS="${SSL_OPTS} -Djavax.net.ssl.trustStorePassword=${SSL_TRUSTSTORE_PASSWORD}"
-    fi
-fi
-
-if [ "${ADDITIONAL_JARS}" == "" ]; then
-  ADDITIONAL_JARS_OPTS=""
-else
-  ADDITIONAL_JARS_OPTS="-a ${ADDITIONAL_JARS}"
-fi
 
 if [ "$USE_JPS" == "true" ]; then
   JPS_RUNNABLE=`$JPS 2>&1`
@@ -90,20 +53,8 @@ else
   PSCMD="$PSJAVA | grep -i jmxtrans | awk '{ print \$2 };'"
 fi
 
-if [ "$CHECK_JAVA" == "true" ]; then
-    JAVA_VERSION=`$JAVA -version 2>&1`
-    if [ $? != 0 ]; then
-        echo "Cannot execute $JAVA!"
-        exit 1
-    fi
-fi
-
-if [ ! -f $JAR_FILE ]; then
-  echo "File not found - $JAR_FILE"
-  exit 1
-fi
-
-start() {
+findpid() {
+	local PID=""
     if [ ! -z "$PIDFILE" ]; then
         if [ -r "$PIDFILE" ]; then
             PID=$(cat $PIDFILE)
@@ -111,19 +62,17 @@ start() {
     else
         PID=$(eval $PSCMD)
     fi
+    echo ${PID}
+}
 
-    if [ ! -z "$PID" ]; then
+start() {
+	local PID=$(findpid)
+    if [ "x${PID}" != "x" ]; then
         echo "jmxtrans appears to already be running @ pid: $PID"
         exit 1
     fi
 
-    if [ -z "$FILENAME" ]; then
-        EXEC=${EXEC:-"-jar $JAR_FILE -e -j $JSON_DIR -s $SECONDS_BETWEEN_RUNS -c $CONTINUE_ON_ERROR $ADDITIONAL_JARS_OPTS"}
-    else
-        EXEC=${EXEC:-"-jar $JAR_FILE -e -f $FILENAME -s $SECONDS_BETWEEN_RUNS -c $CONTINUE_ON_ERROR $ADDITIONAL_JARS_OPTS"}
-    fi
-
-    nohup $JAVA -server $JAVA_OPTS $JMXTRANS_OPTS $GC_OPTS $MONITOR_OPTS ${SSL_OPTS} $EXEC >>$STDOUT_FILE 2>&1 &
+    nohup ${RUN_SCRIPT} >>$STDOUT_FILE 2>&1 &
 
     if [ ! -z "$PIDFILE" ]; then
         echo $! > "$PIDFILE"
@@ -132,24 +81,12 @@ start() {
 }
 
 run() {
-    if [ -z "$FILENAME" ]; then
-        EXEC=${EXEC:-"-jar $JAR_FILE -e -j $JSON_DIR -s $SECONDS_BETWEEN_RUNS -c $CONTINUE_ON_ERROR $ADDITIONAL_JARS_OPTS"}
-    else
-        EXEC=${EXEC:-"-jar $JAR_FILE -e -f $FILENAME -s $SECONDS_BETWEEN_RUNS -c $CONTINUE_ON_ERROR $ADDITIONAL_JARS_OPTS"}
-    fi
-
-    $JAVA -server $JAVA_OPTS $JMXTRANS_OPTS $GC_OPTS $MONITOR_OPTS ${SSL_OPTS} $EXEC
+	${RUN_SCRIPT}
 }
 
 stop() {
-    if [ ! -z "$PIDFILE" ]; then
-        if [ -r "$PIDFILE" ]; then
-            PID=$(cat $PIDFILE)
-        fi
-    else
-        PID=$(eval $PSCMD)
-    fi
-    if [ ! -z "$PID" ]; then
+	local PID=$(findpid)
+    if [ "x${PID}" != "x" ]; then
         kill -15 "$PID"
         cnt=0
         echo -n "Stopping jmxtrans"
@@ -186,14 +123,8 @@ restart() {
 }
 
 status() {
-    if [ ! -z "$PIDFILE" ]; then
-        if [ -r "$PIDFILE" ]; then
-            PID=$(cat $PIDFILE)
-        fi
-    else
-        PID=$(eval $PSCMD)
-    fi
-    if [ ! -z "$PID" ]; then
+	local PID=$(findpid)
+    if [ "x${PID}" != "x" ]; then
         echo "jmxtrans appears to be running at pid: $PID"
         exit 0
     else
