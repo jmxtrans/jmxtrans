0b790e23065aae27f7f978509c75089e68d10ec7
Navgeet Agrawal <navgeet.agrawal@gmail.com>
2018-05-29 17:17:08 +0530

    InfluxDB Writer: Write tags for typeNames instead of prefixing the Field names

--- jmxtrans-output/jmxtrans-output-influxdb/src/main/java/com/googlecode/jmxtrans/model/output/InfluxDbWriter.java
+++ jmxtrans-output/jmxtrans-output-influxdb/src/main/java/com/googlecode/jmxtrans/model/output/InfluxDbWriter.java
@@ -32,6 +32,7 @@ import com.googlecode.jmxtrans.model.Result;
 import com.googlecode.jmxtrans.model.ResultAttribute;
 import com.googlecode.jmxtrans.model.Server;
 import com.googlecode.jmxtrans.model.naming.KeyUtils;
+import com.googlecode.jmxtrans.model.naming.typename.TypeNameValue;
 import org.influxdb.InfluxDB;
 import org.influxdb.InfluxDB.ConsistencyLevel;
 import org.influxdb.dto.BatchPoints;
@@ -42,6 +43,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 import javax.annotation.concurrent.ThreadSafe;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
@@ -69,7 +71,8 @@ public class InfluxDbWriter extends OutputWriterAdapter {
 	@Nonnull private final ConsistencyLevel writeConsistency;
 	@Nonnull private final String retentionPolicy;
 	@Nonnull private final ImmutableMap<String,String> tags;
-	@Nonnull ImmutableList<String> typeNames;
+	@Nonnull private final ImmutableList<String> typeNames;
+	@Nonnull private final ImmutableList<String> typeNamesForTags;
 
 	/**
 	 * The {@link ImmutableSet} of {@link ResultAttribute} attributes of
@@ -79,6 +82,7 @@ public class InfluxDbWriter extends OutputWriterAdapter {
 
 	private final boolean createDatabase;
 	private final boolean reportJmxPortAsTag;
+	private final boolean typeNamesAsTags;
 
 	private final Predicate<Object> isNotNaN = new Predicate<Object>() {
 		@Override
@@ -96,14 +100,17 @@ public class InfluxDbWriter extends OutputWriterAdapter {
 			@Nonnull ImmutableSet<ResultAttribute> resultAttributesToWriteAsTags,
 			@Nonnull ImmutableList<String> typeNames,
 			boolean createDatabase,
-			boolean reportJmxPortAsTag) {
-		this.typeNames = typeNames;
+			boolean reportJmxPortAsTag,
+			boolean typeNamesAsTags) {
+                this.typeNames = typeNames;
+                this.typeNamesForTags = typeNamesAsTags ? typeNames : ImmutableList.<String>of();
 		this.database = database;
 		this.writeConsistency = writeConsistency;
 		this.retentionPolicy = retentionPolicy;
 		this.influxDB = influxDB;
 		this.tags = tags;
 		this.resultAttributesToWriteAsTags = resultAttributesToWriteAsTags;
+		this.typeNamesAsTags = typeNamesAsTags;
 		this.createDatabase = createDatabase;
 		this.reportJmxPortAsTag = reportJmxPortAsTag;
 	}
@@ -207,6 +214,15 @@ public class InfluxDbWriter extends OutputWriterAdapter {
 		Map<String, String> resultTagMap = new TreeMap<>();
 		for (ResultAttribute resultAttribute : resultAttributesToWriteAsTags) {
 			resultAttribute.addTo(resultTagMap, result);
+		}
+
+		if (typeNamesAsTags) {
+			Map<String, String> typeNameValueMap = TypeNameValue.extractMap(resultTagMap.get("typeName"));
+			for (String typeToTag : this.typeNamesForTags) {
+				if (typeNameValueMap.containsKey(typeToTag)) {
+					resultTagMap.put(typeToTag, typeNameValueMap.get(typeToTag));
+				}
+			}
 		}
 
 		return resultTagMap;
