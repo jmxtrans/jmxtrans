1807499701957ddb1ed636a56b54823764fc774e
Rudolf Rakos <r.rakos@focuscura.nl>
2018-07-12 16:48:07 +0200

    Code Review - Fixed old unit and integration tests.

---
+++ jmxtrans-output/jmxtrans-output-cloudwatch/src/test/java/com/googlecode/jmxtrans/model/output/CloudWatchWriterTests.java
@@ -0,0 +1,119 @@
+/**
+ * The MIT License
+ * Copyright © 2010 JmxTrans team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.googlecode.jmxtrans.model.output;
+
+import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchClient;
+import com.amazonaws.services.cloudwatch.model.Dimension;
+import com.amazonaws.services.cloudwatch.model.MetricDatum;
+import com.amazonaws.services.cloudwatch.model.PutMetricDataRequest;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.googlecode.jmxtrans.exceptions.LifecycleException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.googlecode.jmxtrans.model.QueryFixtures.dummyQuery;
+import static com.googlecode.jmxtrans.model.ResultFixtures.dummyResults;
+import static com.googlecode.jmxtrans.model.ServerFixtures.dummyServer;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Tests for {@link com.googlecode.jmxtrans.model.output.CloudWatchWriter}.
+ *
+ * @author <a href="mailto:sascha.moellering@gmail.com">Sascha Moellering</a>
+ */
+@RunWith(MockitoJUnitRunner.StrictStubs.class)
+public class CloudWatchWriterTests {
+
+	@Mock
+	private AmazonCloudWatchClient cloudWatchClient;
+
+	@Captor
+	private ArgumentCaptor<PutMetricDataRequest> requestCaptor;
+
+	private CloudWatchWriter writer;
+
+	@Before
+	public void createCloudWatchWriter() throws Exception {
+		List<Map<String, Object>> dimensions = ImmutableList.<Map<String, Object>>of(
+				ImmutableMap.<String, Object>of("name", "SomeKey", "value", "SomeValue"),
+				ImmutableMap.<String, Object>of("name", "InstanceId", "value", "$InstanceId")
+		);
+
+		writer = new CloudWatchWriter(
+				ImmutableList.<String>of(), false, false,
+				"testNS", dimensions,
+				ImmutableMap.<String, Object>of()) {
+			@Override
+			protected AmazonCloudWatch startAmazonCloudWatch() {
+				return cloudWatchClient;
+			}
+		};
+
+		writer.validateSetup(dummyServer(), dummyQuery());
+
+		writer.start();
+	}
+
+	@Test
+	public void testValidationWithoutSettings() throws Exception {
+		writer.doWrite(dummyServer(), dummyQuery(), dummyResults());
+
+		verify(cloudWatchClient).putMetricData(requestCaptor.capture());
+
+		PutMetricDataRequest request = requestCaptor.getValue();
+		assertThat(request).isNotNull();
+		assertThat(request.getNamespace()).isEqualTo("testNS");
+		assertThat(request.getMetricData()).hasSize(1);
+
+		MetricDatum metricDatum = request.getMetricData().get(0);
+		assertThat(metricDatum.getMetricName()).isEqualTo("ObjectPendingFinalizationCount");
+		assertThat(metricDatum.getValue()).isEqualTo(10);
+		assertThat(metricDatum.getDimensions().size()).isEqualTo(2);
+
+		Dimension dimension0 = metricDatum.getDimensions().get(0);
+		assertThat(dimension0.getName()).isEqualTo("SomeKey");
+		assertThat(dimension0.getValue()).isEqualTo("SomeValue");
+
+		Dimension dimension1 = metricDatum.getDimensions().get(1);
+		assertThat(dimension1.getName()).isEqualTo("InstanceId");
+	}
+
+	@Test
+	public void cloudwatchClientIsClosed() throws LifecycleException {
+		writer.close();
+
+		verify(cloudWatchClient).shutdown();
+	}
+
+}
